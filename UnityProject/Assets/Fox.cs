7933cecb564d126fe92e8eebff32a40b30ca8ef3
OLIVER  HSU <s122672103@gmail.com>
2019-11-22 21:39:56 +0800

    偵測左右方向鍵移動狐狸位置、翻面、新增物理推力

---
+++ UnityProject/Assets/Fox.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    [Header("移動速度")]
+    [Range(0,50)]
+    public float speed = 10f;
+    public Rigidbody2D rig;
+    public SpriteRenderer sr;
+
+
+    private void Update()
+    {
+
+        FoxMove();
+
+    }
+
+    private void FoxMove()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+
+            Vector3 vecR = new Vector3(speed * Time.deltaTime, 0, 0);
+            sr.transform.Translate(vecR);
+            sr.flipX = false;
+            rig.AddForce(new Vector2(speed * Time.deltaTime, 0));
+
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+
+
+            Vector3 vecL = new Vector3(-speed * Time.deltaTime, 0, 0);
+            sr.transform.Translate(vecL);
+            sr.flipX = true;
+            rig.AddForce(new Vector2(-speed * Time.deltaTime, 0));
+        }
+    }
+
+
+}
